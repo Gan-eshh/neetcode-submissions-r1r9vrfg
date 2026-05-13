880b5d720c06fe617de1039f74648ba0233e36b7
Gan-eshh <epganesh349@gmail.com>
2026-05-13 13:00:58 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if(n > m)
+            return false;
+
+        int[] s1fq = new int[26];
+        int[] s2fq = new int[26];
+        for(int i=0;i<n;i++){
+            s1fq[s1.charAt(i)-'a']++;
+            s2fq[s2.charAt(i)-'a']++;
+        }
+
+        if(Arrays.equals(s1fq, s2fq))
+            return true;
+        
+        for(int i=n;i<m;i++){
+            s2fq[s2.charAt(i-n)-'a']--;
+            s2fq[s2.charAt(i)-'a']++;
+            if(Arrays.equals(s1fq, s2fq))   return true;
+        }
+        return false;
+    }
+}

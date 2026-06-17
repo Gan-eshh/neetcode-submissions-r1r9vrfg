3cda5ec53d1acc6935fb17c7101a895acfe25c94
Gan-eshh <epganesh349@gmail.com>
2026-06-17 09:37:30 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> set = new HashSet<>(wordDict);
+
+        int maxLen = 0;
+
+        for(String word : wordDict)
+            maxLen = Math.max(maxLen, word.length());
+        
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+
+        for(int i=1;i<=n;i++){
+            for(int j=i-1 ; j >= Math.max(0, i-maxLen);j--){
+                if(dp[j] && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
